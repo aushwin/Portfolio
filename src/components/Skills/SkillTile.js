710bcaaa021deb4ebb6f809fa5bfa636686ffff8
aushwin <aushwin7biju@gmail.com>
2022-05-17 12:17:29 +0530

    skills added

--- src/components/Skills/SkillTile.js
+++ src/components/Skills/SkillTile.js
@@ -1,12 +1,14 @@
-const SkillTile = ({techStack,title,desc}) =>{
-    return <article className="my-1 bg-tile-bg transition-all hover:important  group-hover:opacity-50  ">
-    <a className="block p-12">
-    <p className="text-xs font-light tracking-widest uppercase text-active-text">{techStack}</p>
-    <h3 className="mt-4 text-2xl font-semibold leading-tight text-active-text">{title}</h3>
-    <p className="mt-4 text-sm text-p-grey">{desc}</p>
+import {FaReact} from 'react-icons/fa'
+
+const SkillTile = ({icon,onhover=''}) =>{
+    return <article className={onhover +" my-1 bg-tile-bg gap-1 transition-all hover:important  group-hover:opacity-50"}>
+    <a className="block p-12 flex items-center justify-center">
+        {icon}
     </a>
 
 </article>
 }
 
 export default SkillTile
+
+
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,16 +1,24 @@
-import SkillTile from "./SkillTile"
+import SkillTile from "./SkillTile";
+import { FaNodeJs,FaReact,FaHtml5,FaCss3Alt,FaNpm,FaYarn,FaGithubSquare,FaPython } from "react-icons/fa";
+import {SiJavascript,SiMongodb,SiCplusplus} from 'react-icons/si'
+import {IoIosGitBranch} from 'react-icons/io'
+const Skills = ({ link }) => {
+  return (
+    <div id={link} className="grid grid-cols-4 gap-3">
+      <SkillTile onhover='hover:text-red-600' icon={<FaHtml5 className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-blue-600' icon={<FaCss3Alt className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-yellow-600'  icon={<SiJavascript className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-blue-400' icon={<FaReact className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-green-600'  icon={<FaNodeJs className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-red-600' icon={<FaNpm className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-blue-400' icon={<FaYarn className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-green-700' icon={<SiMongodb className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-red-600' icon={<IoIosGitBranch className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-white' icon={<FaGithubSquare className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-blue-600' icon={<SiCplusplus className="text-6xl "/>} />
+      <SkillTile onhover='hover:text-yellow-600' icon={<FaPython className="text-6xl "/>} />
+    </div>
+  );
+};
 
-const Skills = ({link})=>{
-    return  <div id={link}>
-    <div className="bg-primary-bg h-24"></div>
-   <SkillTile techStack='Reactjs | Redux | Nodejs | MongoDb | Typescript' title='Gairah-resort' desc='A Resort Management System under development
-by using MERN Stack'/>
-   <SkillTile techStack='Reactjs | Context Api' title='Weatherio' desc='Beautiful Weather App which communicates
-with Weather Api'/>
-<SkillTile techStack='Reactjs | Context Api' title='DevFinder' desc='App with an aesthetically beautiful UI    to find developers in GitHub with the help of
-GitHub API.'/>
-<SkillTile techStack='AngularTs | NodeJs | Internship' title='NodeJs Trainee - CodeLattice' desc='Worked with managing REST APIs with NodeJs'/>
-</div>
-}
-
-export default Skills
+export default Skills;
