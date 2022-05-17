1c31fd476983713aa3be98decd8f7e50136fd3c7
aushwin <aushwin7biju@gmail.com>
2022-05-17 11:34:21 +0530

    link updated - certificates

--- src/components/Certificates/CertificateTile.js
+++ src/components/Certificates/CertificateTile.js
@@ -1,6 +1,6 @@
-const CertificateTile = ({techStack,title,desc}) =>{
+const CertificateTile = ({techStack,title,desc,url}) =>{
     return <article className="my-1 bg-tile-bg transition-all hover:important  group-hover:opacity-50  ">
-    <a className="block p-12">
+    <a href={url} className="block p-12">
     <p className="text-xs font-light tracking-widest uppercase text-active-text">{techStack}</p>
     <h3 className="mt-4 text-2xl font-semibold leading-tight text-active-text" >{title}</h3>
     <p className="mt-4 text-sm text-p-grey">{desc}</p>
--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -2,10 +2,10 @@ import CertificateTile from "./CertificateTile"
 
 const Certificates = ({link})=>{
     return <div id={link} className="grid grid-cols-2 gap-x-2">
-       <CertificateTile techStack='Javascript' title='Javascript - Basics' desc='HackerRank'/>
-        <CertificateTile techStack='' title='Git/Github Certification' desc='Coursera'/>
-       <CertificateTile techStack='Dart | Flutter' title='Flutter/Dart Complete Guide' desc='Udemy'/>
-        <CertificateTile techStack='MongoDB' title='MongoDb - Basics' desc='Mongo University'/>
+       <CertificateTile url='https://www.hackerrank.com/certificates/27db8654f356' techStack='Javascript' title='Javascript - Basics' desc='HackerRank'/>
+        <CertificateTile url='https://www.coursera.org/account/accomplishments/verify/ZRV64EDDCHAU' techStack='' title='Git/Github Certification' desc='Coursera'/>
+       <CertificateTile url='https://www.udemy.com/certificate/UC-86ce52a5-e6eb-454e-8710-9b06156965ff/' techStack='Dart | Flutter' title='Flutter/Dart Complete Guide' desc='Udemy'/>
+        <CertificateTile url='https://university.mongodb.com/course_completion/c3f23092-2ad0-484c-ad41-9a7f909bb4c5?utm_source=copy&utm_medium=social&utm_campaign=university_social_sharing' techStack='MongoDB' title='MongoDb - Basics' desc='Mongo University'/>
     </div>
 }
 
